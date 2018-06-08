2cd7c4752fca296308b1a3cf81449135a0e7622c
Max Knor <mknor@microsoft.com>
2018-06-08 18:23:42 +0200

    Changed SampleApp

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -14,15 +14,20 @@ namespace SampleApp
             TimeBoxDevice box = new TimeBoxDevice();
 
             box.Connect();
-            box.SetView(TimeBoxDevice.ViewType.Clock);
-            box.SetTimeColor(Color.DarkOrchid);
+
+            //box.SetView(TimeBoxDevice.ViewType.Clock);
+            //box.SetTimeColor(Color.DarkOrchid);
             box.SetTime(DateTime.Now);
 
-            box.SetTempUnit(fahrenheit: false);
-            box.SetTempUnitAndColor(Color.Orange, fahrenheit: false);
+            //box.SetTempUnit(fahrenheit: false);
+            //box.SetTempUnitAndColor(Color.Aqua, fahrenheit: false);
 
-            box.SetVolume(0);
-            box.SetVolume(16);
+            //box.SetVolume(0);
+            //box.SetVolume(16);
+
+            box.ShowImage("C:\\Work\\projects\\Knom.TimeBox\\src\\SampleApp\\testdata\\squares.png");
+
+            //box.AnimateImages("C:\\Work\\projects\\Knom.TimeBox\\src\\SampleApp\\testdata\\exp");
 
             Thread.Sleep(Int32.MaxValue);
         }
