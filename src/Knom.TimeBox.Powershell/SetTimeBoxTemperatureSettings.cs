66f51addef75dcff2cd59f566aea9d6209dbfa99
Max Knor <mknor@microsoft.com>
2018-06-14 23:18:12 +0200

    Added Command for Temp Settings.

---
+++ src/Knom.TimeBox.Powershell/SetTimeBoxTemperatureSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Management.Automation;
+
+namespace Knom.TimeBox.Powershell
+{
+    [Cmdlet(VerbsCommon.Set, "TimeBoxTemperatureSettings", SupportsShouldProcess = true)]
+    public class SetTimeBoxTemperatureSettings : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Color Color { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public TemperatureMode Mode { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            var device = TimeBoxContext.Instance.Device;
+
+            device.SetTempUnitAndColor(Color, Mode);
+        }
+    }
+}
