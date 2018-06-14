254c50b36c7f8e911af544b638f34a974cecd4b0
Max Knor <mknor@microsoft.com>
2018-06-14 23:17:57 +0200

    added command for VOLUME

---
+++ src/Knom.TimeBox.Powershell/SetTimeBoxVolume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.PowerShell.Commands;
+
+namespace Knom.TimeBox.Powershell
+{
+    [Cmdlet(VerbsCommon.Set, "TimeBoxVolume", SupportsShouldProcess = true)]
+    public class SetTimeBoxVolume : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0),
+            ValidateRange(0,16)]
+        public int Level { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var device = TimeBoxContext.Instance.Device;
+            device.SetVolume(Level);
+        }
+    }
+}
